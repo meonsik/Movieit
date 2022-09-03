27cf8341760fe54a73ef0fac412dce0a2c2cf1d9
jjnwhy <comsa09@naver.com>
2022-09-03 12:50:41 +0900

    secret키, db정보 따로 빼기

--- movieit/movieit/settings.py
+++ movieit/movieit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import my_setting
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-48_$0j0s0j_s-#qouw(-xdgp2lz8xwlt-cp+v9ls=n6p=vn2@v'
+SECRET_KEY = my_setting.SECRET['secret']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +74,7 @@ WSGI_APPLICATION = 'movieit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = my_setting.DATABASES
 
 
 # Password validation
