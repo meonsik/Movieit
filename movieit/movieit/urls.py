0b2580f5c67c916846ab8d2e6aad361ccfec0b01
wnstm <wnstm0327@gmail.com>
2022-09-13 21:29:44 +0900

    추천

--- movieit/movieit/urls.py
+++ movieit/movieit/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.mainFunc), 
+    path('input', views.inputFunc), 
+    path('recommend_movie', views.recommend_movie), 
 ]
